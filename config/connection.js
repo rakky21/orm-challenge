234456a50bf74f2b3e0cfa24dfdb65be65cabdc2
rakky21 <rakky26@gmail.com>
2021-11-14 09:51:27 -0500

    added the layout of the program and will be testing the product section first

---
+++ config/connection.js
@@ -0,0 +1,33 @@
+require('dotenv').config();
+
+const Sequelize = require('sequelize');
+
+const sequelize = process.env.JAWSDB_URL
+? new Sequelize(process.env.JAWSDB_URL)
+  : new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PW, {
+      host: 'localhost',
+      dialect: 'mysql',
+      dialectOptions: {
+        decimalNumbers: true,
+      },
+    });
+
+module.exports = sequelize;
+
+
+
+
+
+// // import the Sequelize constructor from the library
+// const Sequelize = require('sequelize');
+
+// require('dotenv').config();
+
+// // create connection to our database, pass in your MySQL information for username and password
+// const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PW, {
+//   host: 'localhost',
+//   dialect: 'mysql',
+//   port: 3306
+// });
+
+// module.exports = sequelize;
